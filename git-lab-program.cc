e4d50d5a094306f4dc528c93b39d8804b432480b
Josh Marusek <jm553020@ohio.edu>
2021-09-07 00:57:54 -0400

    add git-lab-program

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Josh Marusek
+ *        Date: 2021-09-07
+ * Description: Displays a greeting
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
